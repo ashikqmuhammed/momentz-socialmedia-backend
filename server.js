1b148ac2291038e02c62d23a341dce79c7b0aced
Ashik <ashikpnc@gmail.com>
2022-11-06 19:00:33 +0530

    changes

--- server.js
+++ server.js
@@ -9,7 +9,17 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  );
+  next();
+});
 
 app.use(
   fileUpload({
