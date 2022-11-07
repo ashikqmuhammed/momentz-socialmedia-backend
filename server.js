e344b1fb6158de37a173987b8841e68831e92e95
Ashik <ashikpnc@gmail.com>
2022-11-07 15:15:53 +0530

    changes

--- server.js
+++ server.js
@@ -47,7 +47,15 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(
   fileUpload({
     useTempFiles: true,
