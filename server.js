79160b3dae1b50494bf1788e112c86e102673419
Ashik <ashikpnc@gmail.com>
2022-11-07 15:05:26 +0530

    changes

--- server.js
+++ server.js
@@ -1,26 +1,58 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+// const { readdirSync } = require("fs");
+// const fileUpload = require("express-fileupload");
+// const dotenv = require("dotenv");
+
+// dotenv.config();
+
+// const app = express();
+// app.use(express.json());
+// const corsOptions = {
+//   origin: "*",
+//   credentials: true, //access-control-allow-credentials:true
+// };
+// app.use(cors());
+
+// app.use(
+//   fileUpload({
+//     useTempFiles: true,
+//   })
+// );
+
+// //routes
+// readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
+
+// //database
+// mongoose
+//   .connect(process.env.DATABASE_URL, {
+//     useNewUrlParser: true,
+//   })
+//   .then(() => console.log("database connected successfully"))
+//   .catch((err) => console.log("error connecting to mongodb", err));
+
+// const PORT = process.env.PORT || 8000;
+// app.listen(PORT, () => {
+//   console.log(`server is running on port ${PORT}..`);
+// });
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const { readdirSync } = require("fs");
 const fileUpload = require("express-fileupload");
+const { readdirSync } = require("fs");
 const dotenv = require("dotenv");
-
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-const corsOptions = {
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-};
 app.use(cors());
-
 app.use(
   fileUpload({
     useTempFiles: true,
   })
 );
-
 //routes
 readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
 
