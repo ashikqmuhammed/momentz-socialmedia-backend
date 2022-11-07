13e6984d146e4e4889676fcc2c3e872397e8116d
Ashik <ashikpnc@gmail.com>
2022-11-07 17:41:24 +0530

    changes

--- server.js
+++ server.js
@@ -38,41 +38,35 @@
 // });
 
 const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
-const { readdirSync } = require("fs");
 const dotenv = require("dotenv");
+
 dotenv.config();
 
-const app = express();
+const { readdirSync } = require("fs");
+
 app.use(express.json());
-// app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors());
 app.use(
   fileUpload({
     useTempFiles: true,
   })
 );
-//routes
+
 readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
 
-//database
+//Database
 mongoose
-  .connect(process.env.DATABASE_URL, {
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
   })
-  .then(() => console.log("database connected successfully"))
-  .catch((err) => console.log("error connecting to mongodb", err));
+  .then(() => console.log("Database connected Succesffully"))
+  .catch((err) => console.log("error connecting MongoDB" + err));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}..`);
+  console.log(`Server Connected ${PORT}`);
 });
